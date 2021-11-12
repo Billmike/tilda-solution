50a299436696da2aee8d975729d11c2332996083
Billmike <ayelegun.kayode@gmail.com>
2021-11-12 19:49:24 +0100

    display quizzes - make quizzes more presentable

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,25 @@ interface IQuiz {
 
 const Home: NextPage<IQuiz> = ({ quizzes }: IQuiz) => {
   return (
-    <div className="container mx-auto">
-      <h2>Tilda Quiz </h2>
+    <div className="container mt-8 mx-auto">
+      <h2 className="text-center text-2xl">Tilda Quizz </h2>
+      <div className="grid mt-8 md:grid-cols-2 gap-4">
       {
         quizzes.quizzes.map(quiz => (
-          <div key={quiz.name}>
-            <h4>{quiz.name}</h4>
+          <div key={quiz.name} className="border-2 p-5 cursor-pointer rounded-md shadow-sm mx-4 md:mx-0">
+            <h4 className="font-bold">{quiz.name}</h4>
+            <div className="flex justify-between mt-3">
+              <div>
+                <p>Score: 8/10</p>
+              </div>
+              <button className="border-2 px-5 text-sm py-1 rounded-md cursor-pointer">
+                Redo
+              </button>
+            </div>
           </div>
         ))
       }
+      </div>
     </div>
   )
 }

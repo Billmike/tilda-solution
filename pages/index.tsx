d61742fec18277e7981e9c9cc120b7bf5f586289
Billmike <ayelegun.kayode@gmail.com>
2021-11-15 01:35:16 +0100

    add more integration tests to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,18 +20,18 @@ const Home: NextPage = () => {
 
   return (
     <div className="container mt-8 mx-auto">
-      <h2 className="text-center text-2xl">Tilda Quizz </h2>
+      <h2 className="text-center text-2xl">Tilda Quizz</h2>
       <div className="grid mt-8 md:grid-cols-2 gap-4">
       {
           quizzes?.map(quiz => (
             <Link key={quiz.id} href={`/quiz/question?id=${quiz.id}`} passHref>
               <div key={quiz.id} className="border-2 p-5 cursor-pointer rounded-md shadow-sm mx-4 md:mx-0">
-            <h4 className="font-bold">{quiz.name}</h4>
-            <div className="flex justify-between mt-3">
-              <div>
+                <h4 className="font-bold">{quiz.name}</h4>
+                <div className="flex justify-between mt-3">
+                  <div>
                     <p>{parseScores[quiz.name] || 'Not started'}</p>
-              </div>
-              <button className="border-2 px-5 text-sm py-1 rounded-md cursor-pointer">
+                  </div>
+                  <button className="border-2 px-5 text-sm py-1 rounded-md cursor-pointer">
                     {parseScores[quiz.name] ? 'Redo' : 'Start'}
                   </button>
                 </div>
